ef951ba5dd0aa541bdb2eb7d0b9c45e1c71c9263
Lakshman Bhupathi <lakshmanbhupathi@gmail.com>
2025-11-27 22:29:22 +0530

    feat: Added CorsConfig

---
+++ src/main/java/com/lakshman/chat_storage_service/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.lakshman.chat_storage_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // in production, replace with specific origins
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
