f80c5d2533b35155a34d1d51807e58688c3557d3
Lakshman Bhupathi <lakshmanbhupathi@gmail.com>
2025-11-27 20:11:15 +0530

    feat: init spring init io

---
+++ src/main/java/com/lakshman/chat_storage_service/ChatStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lakshman.chat_storage_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatStorageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakshman/chat_storage_service/ChatStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshman.chat_storage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
